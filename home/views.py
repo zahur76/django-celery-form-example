c703d8ae37fad5730ab2d2af6e439b423c270e77
zahur <zahurmeerun@hotmail.com>
2022-06-21 11:28:12 +0400

    create: post view to submit form

--- home/views.py
+++ home/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+import time
 
 from .forms import ProfileForm
 
 # Create your views here.
 def index(request):
     """A view to return the home page"""
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+
+        if form.is_valid():
+            print('form submitted')
+            return redirect(reverse("home"))
+        else:
+            print('form invalid')
+            return redirect(reverse("home"))
 
     form = ProfileForm()
 
